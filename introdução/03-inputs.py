f7cedcc5f0facc97bd26d4304ee7629a64153163
Yurematias <yurematias@ufrn.edu.br>
2020-02-24 11:21:30 -0300

    inputs adicionados

---
+++ introdução/03-inputs.py
@@ -0,0 +1,11 @@
+# para ler dados é muito fácil
+variavel = input()
+print(f'A variavel lida é {variavel}')
+# no entando o input sempre retornara uma string 
+# para forçalo a ler int ou outro tipo 
+# precisamos...
+var = int(input())
+print('a variavel {} multiplicada por 2 é = {}'.format(var, var*2))
+
+# também podemos imprimir um valor no momento da leitura para isso...
+idade = int(input('Digite sua idade: ')) 
