543ac09072d1b5cc5621a8625e52944711e737c1
Yure Matias <yurematias@ufrn.edu.br>
2020-10-17 22:52:17 -0300

    conteúdo adicionado

---
+++ 16_bloco_vazio.py
@@ -0,0 +1,20 @@
+# para colocarmos blocos vazios no python precisamos usar o pass
+
+# em outras langs não há esse problema pois podemos usar as {}
+
+# no javascript podemos usar isso tranquilamante:
+# for (let i = 0; i < 10; i++) {
+# 
+# }
+
+# no python não podemos fazer simplesmente:
+# for i in range(10):
+    
+# precisamos usar o pass
+for i in range(10):
+    pass
+
+# o if funciona do mesmo jeito
+
+if True:
+    pass
